39505a5aa50e89b7a6c43a2adc256036a06be1eb
Juliano Petronetto <juliano.petronetto@gmail.com>
2026-01-17 00:54:23 +0100

    fix: update starship configurations for tmux

--- dot_config/zsh/config/startship.zsh
+++ dot_config/zsh/config/startship.zsh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 
-export STARSHIP_THEME="jetpack"
+export STARSHIP_THEME="default"
 
-# if [[ "$TERM_PROGRAM" == "tmux" ]]; then
-#   STARSHIP_THEME="tmux"
-# fi
+if [[ "$TERM_PROGRAM" == "tmux" ]]; then
+  STARSHIP_THEME="tmux"
+fi
 
 export STARSHIP_CONFIG="$XDG_CONFIG_HOME/starship/$STARSHIP_THEME.toml"
 
