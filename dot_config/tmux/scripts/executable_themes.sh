0f8200d3970ea303bf05dc513bccad11511f8d57
Juliano Petronetto <juliano.petronetto@gmail.com>
2026-01-17 03:20:15 +0100

    feat: enhance theme configuration with Gruvbox and default options in tmux

--- dot_config/tmux/scripts/executable_themes.sh
+++ dot_config/tmux/scripts/executable_themes.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-# Gruvbox Hard Theme
+# Tmux Theme Configuration
 
-declare -A THEME=(
+# Gruvbox Hard - hardcoded hex colors
+declare -A GRUVBOX=(
     [background]="#1b1b1b"
     [foreground]="#fbf1c7"
     [black]="#1b1b1b"
@@ -21,5 +22,43 @@ declare -A THEME=(
     [ghgreen]="#b8bb26"
     [ghyellow]="#fabd2f"
 )
+
+# Default - uses terminal's color scheme
+declare -A DEFAULT=(
+    [background]="default"
+    [foreground]="default"
+    [black]="default"
+    [blue]="colour4"
+    [green]="colour2"
+    [purple]="colour5"
+    [red]="colour1"
+    [yellow]="colour3"
+    [white]="colour15"
+    [bblack]="colour0"
+    [bblue]="colour12"
+    [bgreen]="colour10"
+    [bpurple]="colour13"
+    [bred]="colour9"
+    [bwhite]="colour15"
+    [byellow]="colour11"
+    [ghgreen]="colour10"
+    [ghyellow]="colour11"
+)
+
+# Select theme: set @theme in tmux.conf (gruvbox or default)
+THEME_NAME=$(tmux show-option -gqv @theme 2>/dev/null)
+THEME_NAME=${THEME_NAME:-default}
+
+# Copy selected theme to THEME array
+declare -A THEME
+if [[ "$THEME_NAME" == "gruvbox" ]]; then
+    for key in "${!GRUVBOX[@]}"; do
+        THEME[$key]="${GRUVBOX[$key]}"
+    done
+else
+    for key in "${!DEFAULT[@]}"; do
+        THEME[$key]="${DEFAULT[$key]}"
+    done
+fi
 
 RESET="#[fg=${THEME[foreground]},bg=${THEME[background]},nobold,noitalics,nounderscore,nodim]"
