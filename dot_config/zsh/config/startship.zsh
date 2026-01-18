2f431f52dcdfaeb9d9a1bb46a5e29b2c244acc75
Juliano Petronetto <juliano.petronetto@gmail.com>
2026-01-18 18:06:47 +0100

    refactor: remove unused tmux scripts and theme configuration

--- dot_config/zsh/config/startship.zsh
+++ dot_config/zsh/config/startship.zsh
@@ -2,10 +2,6 @@
 
 export STARSHIP_THEME="default"
 
-if [[ "$TERM_PROGRAM" == "tmux" ]]; then
-  STARSHIP_THEME="tmux"
-fi
-
 export STARSHIP_CONFIG="$XDG_CONFIG_HOME/starship/$STARSHIP_THEME.toml"
 
 export STARSHIP_LOG="error"
