11ca5033df9ff5d99a12c6ec4904800746e75de0
Juliano Petronetto <juliano.petronetto@gmail.com>
2026-07-15 13:11:08 +0200

    feat: add privacy configuration for zsh to disable tracking, telemetry, analytics, logging, and crash reporting

---
+++ dot_config/zsh/config/privacy.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+
+# Disable tracking
+export DO_NOT_TRACK=1
+
+# Disable telemetry
+export TELEMETRY_DISABLED=1
+export TELEMETRY_OPT_OUT=1
+export TELEMETRY=0
+
+# Disable usage tracking
+export USAGE_TRACKING_OPT_OUT=1
+
+# Disable analytics
+export ANALYTICS_OPT_OUT=1
+export ANALYTICS=false
+
+# Disable logging
+export LOGGING_OPT_OUT=1
+
+# Disable crash reporting
+export CRASH_REPORTING_OPT_OUT=1
