4952af902435b9f05139235a47e20e3d4515ef76
Juliano Petronetto <juliano.petronetto@gmail.com>
2026-07-23 10:22:38 +0200

    chore(zsh): remove duplicated zoxide initialization

--- dot_config/zsh/config/zoxide.zsh
+++ dot_config/zsh/config/zoxide.zsh
@@ -2,8 +2,3 @@
 
 # Zoxide data directory
 export _ZO_DATA_DIR="$XDG_DATA_HOME/zoxide"
-
-# Zoxide initialization (installed via zinit)
-if command -v zoxide &> /dev/null; then
-  eval "$(zoxide init zsh)"
-fi
